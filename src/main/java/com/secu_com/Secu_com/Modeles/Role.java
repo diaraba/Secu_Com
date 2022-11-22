0060d79e3ecdcd84da1e1e7edb643011ed9973d1
diaraba <diarradiaraba@gmail.com>
2022-11-22 15:52:58 +0000

    first commit

---
+++ src/main/java/com/secu_com/Secu_com/Modeles/Role.java
@@ -0,0 +1,17 @@
+package com.secu_com.Secu_com.Modeles;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Table(name = "role")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(length = 60)
+    private String name;
+}
---
+++ src/main/java/com/secu_com/Secu_com/SecuComApplication.java
@@ -0,0 +1,13 @@
+package com.secu_com.Secu_com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecuComApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecuComApplication.class, args);
+	}
+
+}
